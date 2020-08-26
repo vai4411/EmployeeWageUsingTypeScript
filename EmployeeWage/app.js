41c0a85c4c14185aa133a0e3c33568b812f6d9bb
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-08-27 01:28:19 +0530

    [Vaibhav]ADDED: used working hours and total days for calculate monthly wage

--- EmployeeWage/app.js
+++ EmployeeWage/app.js
@@ -5,30 +5,33 @@ const DAY_PER_MONTH = 20;
 let day = 0;
 let totalWage = 0;
 let dailyWage = 0;
+let workingHours = 0;
+let totalHours = 0;
 let calculateDailyWage = (empCheck) => {
     switch (empCheck) {
         case 1:
-            console.log("Employee present for full time");
             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            workingHours = FULL_DAY_HOUR;
             break;
         case 2:
-            console.log("Employee present for part time");
             dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+            workingHours = PART_TIME_HOUR;
             break;
         default:
-            console.log("Employee is absent");
             dailyWage = 0;
+            workingHours = 0;
             break;
     }
     return dailyWage;
 };
 let monthlyWage = () => {
-    while (day < 20) {
+    while (day < 20 && totalHours < 100) {
         let empCheck = Math.round(Math.random() * 2);
         totalWage += calculateDailyWage(empCheck);
+        totalHours += workingHours;
         day++;
     }
     return totalWage;
 };
-console.log("Employee daily wage is " + monthlyWage());
+console.log("Employee monthly wage is " + monthlyWage());
 //# sourceMappingURL=app.js.map
--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -5,34 +5,37 @@ const DAY_PER_MONTH: number = 20;
 let day: number = 0;
 let totalWage: number = 0;
 let dailyWage: number = 0;
+let workingHours: number = 0;
+let totalHours: number = 0;
 
 let calculateDailyWage = (empCheck: number): number => {
     switch (empCheck) {
         case 1:
-            console.log("Employee present for full time");
             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            workingHours = FULL_DAY_HOUR;
             break;
         case 2:
-            console.log("Employee present for part time");
             dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+            workingHours = PART_TIME_HOUR; 
             break;
         default:
-            console.log("Employee is absent");
             dailyWage = 0;
+            workingHours = 0;
             break;
     }
     return dailyWage;
 }
 
 let monthlyWage = (): number => {
-    while (day < 20) {
+    while (day < 20 && totalHours < 100) {
         let empCheck: number = Math.round(Math.random() * 2);
         totalWage += calculateDailyWage(empCheck);
+        totalHours += workingHours;
         day++;
     }
     return totalWage;
 }
 
-console.log("Employee daily wage is " + monthlyWage());
+console.log("Employee monthly wage is " + monthlyWage());
 
 
