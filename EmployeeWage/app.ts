c85bf6d00abdc62667cbfdfe563d6b433fdb72cd
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-08-27 00:15:34 +0530

    [Vaibhav]ADDED: used part time hours for calculate part time wage

--- EmployeeWage/app.ts
+++ EmployeeWage/app.ts
@@ -1,10 +1,16 @@
 let isPresent : number = 1;
 let WAGE_PER_HOUR = 20;
 let FULL_DAY_HOUR = 8;
-let empCheck : number = Math.round(Math.random());
+let PART_TIME_HOUR = 4;
+
+let empCheck : number = Math.round(Math.random() * 10) % 3;
 if (empCheck == 1) {
     var dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
     console.log("Employee daily wage is " + dailyWage);
+}
+else if (empCheck == 2) {
+    var dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+    console.log('Part time employee wage is : ' + dailyWage);
 }
 else {
     console.log("Enployee is absent");
